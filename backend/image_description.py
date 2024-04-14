f56f7aac368392f0e832295dadac2e02adac301b
MagaDaur <magarulit2014@gmail.com>
2024-04-14 03:48:52 +0300

    faster image_description

--- backend/image_description.py
+++ backend/image_description.py
@@ -1,4 +1,3 @@
-
 from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration, T5ForConditionalGeneration, T5Tokenizer
 
@@ -7,16 +6,16 @@ device = 'cpu'
 processor = BlipProcessor.from_pretrained('Salesforce/blip-image-captioning-base')
 model_blip = BlipForConditionalGeneration.from_pretrained('Salesforce/blip-image-captioning-base').to(device)
 
-model_t5 = T5ForConditionalGeneration.from_pretrained('utrobinmv/t5_translate_en_ru_zh_large_1024').to(device)
-tokenizer_t5 = T5Tokenizer.from_pretrained('utrobinmv/t5_translate_en_ru_zh_large_1024')
+model_t5 = T5ForConditionalGeneration.from_pretrained('utrobinmv/t5_translate_en_ru_zh_small_1024').to(device)
+tokenizer_t5 = T5Tokenizer.from_pretrained('utrobinmv/t5_translate_en_ru_zh_small_1024')
 
 def generate_image_description(file_path: str):
-    image = Image.open(file_path).convert("RGB")
+    image = Image.open(file_path)
 
     inputs = processor(images=image, return_tensors="pt").to(device)
-    outputs = model_blip.generate(**inputs)
+    outputs = model_blip.generate(**inputs, max_new_tokens=20)
     caption = processor.decode(outputs[0], skip_special_tokens=True)
 
-    input_ids = tokenizer_t5(caption, return_tensors="pt").input_ids.to(device)
+    input_ids = tokenizer_t5('translate to ru: ' + caption, return_tensors="pt").input_ids.to(device)
     generated_tokens = model_t5.generate(input_ids)
     return tokenizer_t5.decode(generated_tokens[0], skip_special_tokens=True)
